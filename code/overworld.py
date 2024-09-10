d1ae9dd492f2d246ac102e0e2fc4ca680c978d20
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-09-11 05:57:07 +0700

    overworld movement

--- code/overworld.py
+++ code/overworld.py
@@ -10,8 +10,11 @@ class OverWorld():
         
         # groups
         self.all_sprites = WorldSprites(self.data)
+        self.node_sprites = pygame.sprite.Group()
         
         self.setup(tmx_map, overworld_frames)
+        
+        self.current_node = [node for node in self.node_sprites if node.level == 0][0]
         
     def setup(self, tmx_map, overword_frames):
         # water
@@ -33,6 +36,14 @@ class OverWorld():
                 z = Z_LAYERS[f"{'bg details' if obj.name == 'grass' else 'bg tiles'}"]
                 Sprite((obj.x, obj.y), obj.image,self.all_sprites,z)
                 
+        # paths
+        self.paths = {}
+        for obj in tmx_map.get_layer_by_name('Paths'):
+            pos = [(int(p.x + TILE_SIZE / 2), int(p.y + TILE_SIZE / 2)) for p in obj.points]
+            start = obj.properties['start']
+            end = obj.properties['end']
+            self.paths[end] = {'pos': pos, 'start': start}
+                
         # nodes & player
         for obj in tmx_map.get_layer_by_name('Nodes'):
             # player
@@ -40,8 +51,28 @@ class OverWorld():
                 self.icon = Icon((obj.x + TILE_SIZE/2, obj.y + TILE_SIZE/2), self.all_sprites, overword_frames['icon'])
             # nodes
             if obj.name == 'Node':
-                Node((obj.x, obj.y), overword_frames['path']['node'],self.all_sprites,obj.properties['stage'], self.data)
+                available_paths = {k:v for k, v in obj.properties.items() if k in ('left', 'right', 'up', 'down')}
+                Node((obj.x, obj.y), overword_frames['path']['node'],[self.all_sprites, self.node_sprites],obj.properties['stage'], self.data, available_paths)
+    
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if self.current_node:
+            if keys[pygame.K_DOWN] and self.current_node.can_move('down'):
+                self.move('down')
+                
+    def move(self, direction):
+        path_key = int(self.current_node.paths[direction][0])
+        path_reverse = self.current_node.paths[direction][-1] == 'r'
+        path = self.paths[path_key]['pos'][:] if not path_reverse else self.paths[path_key]['pos'][::-1]
+        self.icon.start_move(path)
+        
+    def get_current_node(self):
+        nodes = pygame.sprite.spritecollide(self.icon, self.node_sprites, False)
+        if nodes:
+            self.current_node = nodes[0]
     
     def run(self, dt):
+        self.input()
+        self.get_current_node()
         self.all_sprites.update(dt)
         self.all_sprites.draw(self.icon.rect.center)
